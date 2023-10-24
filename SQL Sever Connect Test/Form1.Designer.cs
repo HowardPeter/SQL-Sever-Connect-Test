f70faa2bf2bac6b2e7f5405b38b7af5ea1aebcc7
HowardPeter <phuocnt1611@gmail.com>
2023-10-25 03:35:15 +0700

    Test git on VS

--- SQL Sever Connect Test/Form1.Designer.cs
+++ SQL Sever Connect Test/Form1.Designer.cs
@@ -95,7 +95,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Window;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
             this.label1.Location = new System.Drawing.Point(125, 64);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(147, 25);
